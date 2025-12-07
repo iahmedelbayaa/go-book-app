1a9c28f4f3ccc2d181d6f3d6401dc9617896a940
iahmedelbayaa <iahmedelbayaa>
2025-12-08 01:17:00 +0200

    Add database configuration and connection setup

---
+++ config/db.config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/iahmedelbayaa/go-book-app/models"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found, using environment variables")
+	}
+
+	// Build DSN from environment variables
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
+	)
+	
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	DB = database
+	fmt.Println("Database connected successfully!")
+
+	// Auto-migrate models to create tables
+	err = DB.AutoMigrate(&models.Book{}, &models.User{})
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+	fmt.Println("Database migration completed!")
+}
